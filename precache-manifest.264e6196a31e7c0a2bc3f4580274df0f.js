52b8bf3cb0173207848849df79ae19c4fe5a102b
Sherwin Aborot <caskispace@gmail.com>
2019-05-03 19:13:12 +0800

    Updates

---
+++ precache-manifest.264e6196a31e7c0a2bc3f4580274df0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b466fb670b647dc51dd4d8d9625d9a11",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/Robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "a69d246acd9e336f7909",
+    "url": "/Robofriends/static/css/main.b96ce9c9.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/Robofriends/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "a69d246acd9e336f7909",
+    "url": "/Robofriends/static/js/main.4fb2d189.chunk.js"
+  },
+  {
+    "revision": "47afcf7c1fdce8fc928a",
+    "url": "/Robofriends/static/js/runtime~main.4fea8ee0.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
